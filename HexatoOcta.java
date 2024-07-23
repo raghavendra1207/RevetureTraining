15460150abecea173008be52a63a5e1944be691e
raghavendra1207 <112899843+raghavendra1207@users.noreply.github.com>
2024-07-23 11:33:33 +0530

    Update1

---
+++ HexatoOcta.java
@@ -0,0 +1,10 @@
+public class  HexatoOcta
+  {
+    public static void main(String args[])
+    {
+      int a=0x1F;
+      String ab=a.toString();
+      System.out.println(ab);
+      
+    }  
+  }
